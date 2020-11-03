normalization fixed

src/app_helper.py
@@ -27,7 +27,7 @@ QUANTIFIER_ALIASES = {
     'svmkld': lambda learner: OneVsAllELM(settings.SVM_PERF_HOME, loss='kld'),
     'svmnkld': lambda learner: OneVsAllELM(settings.SVM_PERF_HOME, loss='nkld'),
     'svmmae': lambda learner: OneVsAllELM(settings.SVM_PERF_HOME, loss='mae'),
-    'svmmrae': lambda learner: OneVsAllELM(settings.SVM_PERF_HOME, loss='mrae'),
+    'svmmrae': lambda learner: OneVsAllELM(settings.SVM_PERF_HOME, loss='mrae', C=0.0001),
     'mlpe': lambda learner: MaximumLikelihoodPrevalenceEstimation(),
 }
 
@@ -239,8 +239,7 @@ def produce_predictions_ELM(method:OneVsAllELM, test):
 
     def test_method(sample):
         true_prevalence = sample.prevalence()
-        estim_prevalence = sample.documents.mean(axis=0)
-        estim_prevalence /= estim_prevalence.sum()
+        estim_prevalence = normalize_prevalence(sample.documents.mean(axis=0))
         return true_prevalence, estim_prevalence
 
     results = Parallel(n_jobs=-1)(

src/quapy/method/aggregative.py
@@ -256,8 +256,7 @@ class OneVsAllELM(AggregativeQuantifier):
                 delayed(binary_quant_task)(c, self.learners, X) for c in self.learners.keys()
             )
         )
-        prevalences /= prevalences.sum()
-        return prevalences
+        return normalize_prevalence(prevalences)
 
     @property
     def classes_(self):
@@ -268,7 +267,7 @@ class OneVsAllELM(AggregativeQuantifier):
         for class_ in data.classes_:
             classifications.append(self.learners[class_].predict(data.documents))
         classifications = np.vstack(classifications).T
-        precomputed =  LabelledCollection(classifications, data.labels)
+        precomputed = LabelledCollection(classifications, data.labels)
         return precomputed
 
     def set_params(self, **parameters):